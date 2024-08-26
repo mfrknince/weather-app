9c9b0e67d67ee8180c51e3bd0cf867cc1dc4e6de
M.Furkan İNCE <mfrknince@gmail.com>
2024-08-26 17:23:58 +0300

    added chart to UI, added query func, added some validation

--- weather-app-ui.py
+++ weather-app-ui.py
@@ -3,7 +3,7 @@ author = @mfrknince on social
 '''
 
 from weatherApp import WeatherApp
-import pandas as pd
+import plotly.graph_objs as go
 import streamlit as st
 import time
 
@@ -15,85 +15,112 @@ placeholder = st.empty()
 
 result = st.container()
 
-
-CityCol, TempCol, HumCol, WindCol = st.columns([3,3,2,2])
+CityCol, TempCol, HumCol, WindCol = st.columns([3, 3, 2, 2])
 with input_ui:
-
     city_name = st.text_input('Select your Area')
 
 if show:
-    with result:
+    if city_name:
+        with result:
 
-        wApp = WeatherApp(city_name)
+            wApp = WeatherApp(city_name)
 
-        wApp.find_coordinates()
-        with st.spinner('Wait for it...'):
-
-            wApp.store_weather_daily_data()
-            placeholder.success('Data correctly uploaded to InfluxDB')
-            time.sleep(3)
+            wApp.find_coordinates()
+            with st.spinner('Wait for it...'):
+                placeholder.info('Weather data getting from OpenWeatherMap...')
+                time.sleep(2)
+                placeholder.empty()
+                placeholder.info('Weather data uploading to InfluxDB')
+                time.sleep(2)
+                wApp.store_weather_daily_data()
+                placeholder.empty()
+                placeholder.success('Data correctly uploaded to InfluxDB')
+                time.sleep(3)
+                placeholder.empty()
+                placeholder.info('Getting data from InfluxDB')
+                wApp.get_weather_daily_data_from_db()
+                time.sleep(3)
             placeholder.empty()
-            placeholder.info('Getting data from InfluxDB')
-            wApp.get_weather_daily_data_from_db()
-            time.sleep(3)
-        placeholder.empty()
-        placeholder.success("Done!")
-        placeholder.empty()
+            placeholder.success("Done!")
+            placeholder.empty()
 
+            # current
+            with st.container():
+                with CityCol:
+                    st.title(wApp.city_name)
+                    st.caption(str(wApp.current_pivot_df.iloc[0]['time'].strftime('%m/%d/%Y')))
 
-        with st.container():
-            with CityCol:
-                st.title(wApp.city_name)
-                st.caption(str(wApp.current_pivot_df.iloc[0]['time'].strftime('%m/%d/%Y')))
+                with TempCol:
+                    icon, info = st.columns([1, 3])
 
-            with TempCol:
-                icon, info = st.columns([1,3])
-
-                with icon:
-                    icon = str(wApp.current_pivot_df.iloc[0]['icon'])
-                    icon = '0' + icon.replace('.0','d') + '.png'
-                    st.image('icons/'+icon)
-
-                with info:
-                    st.caption('Temperature')
-                    st.subheader(str((round(wApp.current_pivot_df.iloc[0]['temp'])))+ str('°C'))
-
-
-            with HumCol:
-                st.caption('Humidity')
-                st.subheader(str(wApp.current_pivot_df.iloc[0]['humidity']) + str(' %'))
-
-            with WindCol:
-                st.caption('Wind Speed')
-                st.subheader(str(wApp.current_pivot_df.iloc[0]['wind_speed']) + str(' km/h'))
-
-    with st.spinner():
-
-        columns = st.columns(5)
-
-        for i, col in enumerate(columns):
-            with col:
-                with st.container(border=True):
-                    st.subheader(str(wApp.weekly_pivot_df.iloc[i+1]['time'].strftime('%m/%d/%Y')))
-                    st.write(str(round(wApp.weekly_pivot_df.iloc[i+1]['max'])) + str('°C') + "  -  " + str(round(wApp.weekly_pivot_df.iloc[i+1]['min'])) + str('°C'))
-
-                    col1,iconCol,col2 = st.columns([1,2,1])
-                    with iconCol:
-                        icon = str(wApp.weekly_pivot_df.iloc[i+1]['icon'])
+                    with icon:
+                        icon = str(wApp.current_pivot_df.iloc[0]['icon'])
                         icon = '0' + icon.replace('.0', 'd') + '.png'
-                        st.image('icons/' + icon, width=75)
+                        st.image('icons/' + icon)
 
-                    hum, wind = st.columns(2)
+                    with info:
+                        st.caption('Temperature')
+                        st.subheader(str((round(wApp.current_pivot_df.iloc[0]['temp']))) + str('°C'))
+                        st.caption(str((round(wApp.current_pivot_df.iloc[0]['feels_like']))) + str('°C Feels'))
 
-                    with hum:
-                        st.caption('Humidity')
-                        st.write(str(wApp.weekly_pivot_df.iloc[i+1]['humidity']) + str(' %'))
+                with HumCol:
+                    st.caption('Humidity')
+                    st.subheader(str(wApp.current_pivot_df.iloc[0]['humidity']) + str(' %'))
 
-                    with wind:
-                        st.caption('Wind Speed')
-                        st.write(str(wApp.weekly_pivot_df.iloc[i+1]['wind_speed']) + str(' km/h'))
+                with WindCol:
+                    st.caption('Wind Speed')
+                    st.subheader(str(wApp.current_pivot_df.iloc[0]['wind_speed']) + str(' km/h'))
+
+        with st.spinner():
+            # forecast
+            columns = st.columns(5)
+
+            for i, col in enumerate(columns):
+                with col:
+                    with st.container(border=True):
+                        st.subheader(str(wApp.weekly_pivot_df.iloc[i + 1]['time'].strftime('%m/%d/%Y')))
+                        st.write(str(round(wApp.weekly_pivot_df.iloc[i + 1]['max'])) + str('°C') + "  -  " + str(
+                            round(wApp.weekly_pivot_df.iloc[i + 1]['min'])) + str('°C'))
+
+                        col1, iconCol, col2 = st.columns([1, 2, 1])
+                        with iconCol:
+                            icon = str(wApp.weekly_pivot_df.iloc[i + 1]['icon'])
+                            icon = '0' + icon.replace('.0', 'd') + '.png'
+                            st.image('icons/' + icon, width=75)
+
+                        hum, wind = st.columns(2)
+
+                        with hum:
+                            st.caption('Humidity')
+                            st.write(str(wApp.weekly_pivot_df.iloc[i + 1]['humidity']) + str(' %'))
+
+                        with wind:
+                            st.caption('Wind Speed')
+                            st.write(str(wApp.weekly_pivot_df.iloc[i + 1]['wind_speed']) + str(' km/h'))
+
+            fig = go.Figure()
+
+            fig.add_trace(
+                go.Scatter(x=wApp.weekly_pivot_df['time'], y=wApp.weekly_pivot_df['max'], mode='lines',
+                           name='Max Temperature',
+                           line=dict(color='red')))
+            fig.add_trace(
+                go.Scatter(x=wApp.weekly_pivot_df['time'], y=wApp.weekly_pivot_df['min'], mode='lines',
+                           name='Min Temperature',
+                           line=dict(color='blue')))
+            fig.add_trace(
+                go.Scatter(x=wApp.weekly_pivot_df['time'], y=wApp.weekly_pivot_df['day'], mode='lines',
+                           name='Day Temperature',
+                           line=dict(color='green')))
+
+            fig.update_layout(title='Daily Temperatures',
+                              xaxis_title='Date',
+                              yaxis_title='Temperature')
+
+            st.plotly_chart(fig)
+    else:
+        st.error('Please select your area')
 
 with st.expander('developed by M. Furkan Ince'):
     st.markdown('This project is open to all kinds of improvements')
     st.markdown('if you would like to contact: mfrknince@gmail.com or :gray-background[@mfrknince] on social ')
-
--- weatherApp.py
+++ weatherApp.py
@@ -4,12 +4,20 @@ author = @mfrknince on social
 
 import requests
 import time, json
+
+import streamlit
 from influxdb_client import InfluxDBClient, Point, WriteOptions
 import pandas as pd
-#!pip install tabulate
+
+
+# pip install tabulate
+
+# pip install -r requirements.txt
+
+#TODO MORE CLEAN CODE
 
 class WeatherApp:
-    def __init__(self,city_name):
+    def __init__(self, city_name):
         self.weather_key = "2d300cd42548efadf84eb44c352f98cf"
         self.weather_url = "http://api.openweathermap.org"
         self.db_token = "fBAUCTCV-fhFJP-5O-2OX7wCjuLdGXAL2XiLsHqD2wj1ZFkS-_uWXC9oqTE2PK6-UpAv74uennRGTJDEBKYWqA=="
@@ -19,48 +27,54 @@ class WeatherApp:
         self.city_name = city_name
         self.city_lat = 0
         self.city_lon = 0
-        self.point_name ="weather_data"
+        self.point_name = "weather_data"
         self.client = self.create_client()
-        self.weekly_pivot_df,self.current_pivot_df =pd.DataFrame(),pd.DataFrame()
-
+        self.weekly_pivot_df, self.current_pivot_df = pd.DataFrame(), pd.DataFrame()
 
     def get_weather_daily_data_from_db(self):
         return self.get_data_from_db()
 
-
-
     def find_coordinates(self):
 
         print('Welcome to Weather App')
-        print('\n'*5)
+        print('\n' * 5)
         url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.city_name}&appid={self.weather_key}'
-
 
         res = requests.get(url)
         data = res.json()
-
 
         self.city_name = data[0]['name']
         self.city_lat = data[0]['lat']
         self.city_lon = data[0]['lon']
 
-
     def get_weather_daily_data(self):
         df_current = None
+        df_daily = None
 
         url = f'https://api.openweathermap.org/data/3.0/onecall?lat={self.city_lat}&lon={self.city_lon}&units=metric&appid={self.weather_key}'
 
         res = requests.get(url)
         data = res.json()
 
-        df_current = pd.DataFrame(data['current'])
-        df_daily = pd.DataFrame(data['daily'])
+        if isinstance(data['current'], dict):
+            df_current = pd.DataFrame([data['current']])
+        elif isinstance(data['current'], list):
+            df_current = pd.DataFrame(data['current'])
+        else:
+            raise ValueError("data['current'] should be a list or a dict")
 
-        df_current['icon'] = df_current.iloc[0]['weather']['icon'][:-1]
+        if isinstance(data['daily'], dict):
+            df_daily = pd.DataFrame([data['daily']])
+        elif isinstance(data['daily'], list):
+            df_daily = pd.DataFrame(data['daily'])
+        else:
+            raise ValueError("data['daily'] should be a list or a dict")
+
+        df_current['icon'] = df_current.iloc[0]['weather'][0]['icon'][:-1]
         df_daily['icon'] = df_daily['weather'].apply(lambda x: x[0]['icon'][:-1]).to_frame(name='icon')
 
-        df_current = df_current[['dt', 'temp', 'wind_speed', 'humidity','icon']]
-        df_daily = df_daily[['dt', 'temp', 'wind_speed', 'humidity','icon']]
+        df_current = df_current[['dt', 'temp', 'wind_speed', 'humidity', 'feels_like', 'icon']]
+        df_daily = df_daily[['dt', 'temp', 'wind_speed', 'humidity', 'icon']]
 
         df_daily['city'] = self.city_name
         df_current['city'] = self.city_name
@@ -72,9 +86,30 @@ class WeatherApp:
         df_daily['datetime'] = df_daily['datetime'].dt.strftime('%Y-%m-%d')
 
         df_current['icon'] = df_daily['icon'].astype(float)
-        df_daily['icon']=df_daily['icon'].astype(float)
+        df_daily['icon'] = df_daily['icon'].astype(float)
 
-        return df_current,df_daily
+        return df_current, df_daily
+
+    def analyse_weather_data(self, df_current, df_daily):
+
+        print('Current data has ' + str(df_current.isna().sum().sum()) + ' NaN values')
+        print('Current data has ' + str(df_daily.isna().sum().sum()) + ' NaN values')
+
+        if df_current.isna().sum().sum() > 0:
+            streamlit.error('Please check your current weather data')
+
+        if df_daily.isna().sum().sum() > 0:
+            df_daily.fillna(df_daily.mean(), inplace=True)
+
+        print('Here your current df info')
+        print(df_current.info())
+        print(df_current.describe())
+
+        print('\n' * 3)
+
+        print('Here your daily df info')
+        print(df_daily.info())
+        print(df_daily.describe())
 
     def create_client(self):
 
@@ -88,8 +123,7 @@ class WeatherApp:
 
     def modify_daily_data(self):
 
-
-        self.current_data,self.weather_daily_data= self.get_weather_daily_data()
+        self.current_data, self.weather_daily_data = self.get_weather_daily_data()
 
         current_data_json = self.current_data.to_json(orient='records')
         weekly_data_json = self.weather_daily_data.to_json(orient='records')
@@ -105,7 +139,7 @@ class WeatherApp:
             "min": [],
             "time": [],
             "wind_speed": [],
-            "icon":[]
+            "icon": []
         }
 
         for entry in weekly_data_dict:
@@ -121,19 +155,26 @@ class WeatherApp:
         self.weather_daily_data = data
         self.weather_daily_data_df = pd.DataFrame(data=self.weather_daily_data)
 
+        self.weather_daily_data_df['day'] = self.weather_daily_data_df['day'].clip(lower=-100, upper=60)
+        self.weather_daily_data_df['max'] = self.weather_daily_data_df['max'].clip(lower=-100, upper=60)
+        self.weather_daily_data_df['min'] = self.weather_daily_data_df['min'].clip(lower=-100, upper=60)
+        self.weather_daily_data_df['humidity'] = self.weather_daily_data_df['humidity'].clip(lower=0, upper=100)
+        self.weather_daily_data_df['wind_speed'] = self.weather_daily_data_df['wind_speed'].clip(lower=0, upper=200)
+
         data2 = {
             "city": [],
             "temp": [],
+            "feels_like": [],
             "humidity": [],
             "time": [],
             "wind_speed": [],
-            "icon":[]
+            "icon": []
         }
-
 
         for entry in current_data_dict:
             data2["city"].append(entry['city'].split(' ')[0])
             data2["temp"].append(entry['temp'])
+            data2["feels_like"].append(entry['feels_like'])
             data2["humidity"].append(entry['humidity'])
             data2["time"].append(entry['datetime'])
             data2["wind_speed"].append(entry['wind_speed'])
@@ -142,17 +183,21 @@ class WeatherApp:
         self.weather_current_data = data2
         self.weather_current_data_df = pd.DataFrame(data=self.weather_current_data)
 
-        return self.weather_current_data_df,self.weather_daily_data_df
+        self.weather_current_data_df['temp'] = self.weather_current_data_df['temp'].clip(lower=-100, upper=60)
+        self.weather_current_data_df['feels_like'] = self.weather_current_data_df['feels_like'].clip(lower=-100,
+                                                                                                     upper=60)
+        self.weather_current_data_df['humidity'] = self.weather_current_data_df['humidity'].clip(lower=0, upper=100)
+        self.weather_current_data_df['wind_speed'] = self.weather_current_data_df['wind_speed'].clip(lower=0, upper=200)
 
+        self.analyse_weather_data(self.weather_current_data_df, self.weather_daily_data_df)
+
+        return self.weather_current_data_df, self.weather_daily_data_df
 
     def store_weather_daily_data(self):
 
-
-        current_df,weekly_df = self.modify_daily_data()
-
+        current_df, weekly_df = self.modify_daily_data()
 
         write_api = self.client.write_api(write_options=WriteOptions(batch_size=10, flush_interval=10))
-
 
         for index, row in weekly_df.iterrows():
             point = Point(self.point_name) \
@@ -162,7 +207,7 @@ class WeatherApp:
                 .field("max", row["max"]) \
                 .field("min", row["min"]) \
                 .field("wind_speed", row["wind_speed"]) \
-                .field("icon",row["icon"])\
+                .field("icon", row["icon"]) \
                 .time(row["time"])
             print(point)
             write_api.write(bucket=self.db_name, org=self.db_org, record=point)
@@ -170,19 +215,34 @@ class WeatherApp:
         time.sleep(10)
 
         for index, row in current_df.iterrows():
-            point = Point(self.point_name+'_current') \
+            point = Point(self.point_name + '_current') \
                 .tag("city", row["city"]) \
                 .field("temp", float(row["temp"])) \
+                .field("feels_like", float(row["feels_like"])) \
                 .field("humidity", row["humidity"]) \
                 .field("wind_speed", row["wind_speed"]) \
-                .field("icon",row["icon"]) \
+                .field("icon", row["icon"]) \
                 .time(row["time"])
             print(point)
             write_api.write(bucket=self.db_name, org=self.db_org, record=point)
 
-
-
         print("Complete. Return to the InfluxDB UI.")
+
+    def query_with_retries(self, query_api, query, max_retries=5, wait_time=2):
+        retries = 0
+        result_current = None
+
+        while retries < max_retries:
+            result_current = query_api.query(org=self.db_org, query=query)
+
+            if len(result_current) > 0:
+                return result_current
+
+            retries += 1
+            print(f"No results found. Retrying in {wait_time} seconds...")
+            time.sleep(wait_time)
+
+        raise Exception("Max retries exceeded. No results found.")
 
     def get_data_from_db(self):
 
@@ -190,22 +250,13 @@ class WeatherApp:
 
         query = f"""
         from(bucket: "{self.db_name}")
-          |> range(start: 2024-08-01T00:00:00Z, stop: 2024-08-31T23:59:59Z)
+          |> range(start: -1d, stop: 6d)
           |> filter(fn: (r) => r["_measurement"] == "{self.point_name}")
            |> filter(fn: (r) => r["city"] == "{self.city_name.split(' ')[0]}")
         """
 
-        query_curr = f"""
-                from(bucket: "{self.db_name}")
-                  |> range(start: -2d)
-                  |> filter(fn: (r) => r["_measurement"] == "{self.point_name+'_current'}")
-                   |> filter(fn: (r) => r["city"] == "{self.city_name.split(' ')[0]}")
-                """
+        result = self.query_with_retries(query_api, query)
 
-        result = query_api.query(org=self.db_org, query=query)
-
-
-        #weekly
         records = []
         for table in result:
             for record in table.records:
@@ -220,11 +271,14 @@ class WeatherApp:
 
         pivot_df = df.pivot_table(index='time', columns='field', values='value', aggfunc='mean').reset_index()
 
-        result_current = query_api.query(org=self.db_org, query=query_curr)
+        query_curr = f"""
+                        from(bucket: "{self.db_name}")
+                          |> range(start: -24h)
+                          |> filter(fn: (r) => r["_measurement"] == "{self.point_name + '_current'}")
+                           |> filter(fn: (r) => r["city"] == "{self.city_name.split(' ')[0]}")
+                        """
 
-        while len(result_current)<1:
-            result_current = query_api.query(org=self.db_org, query=query_curr)
-
+        result_current = self.query_with_retries(query_api, query_curr)
 
         if result_current:
             records_curr = []
@@ -237,7 +291,6 @@ class WeatherApp:
                     field_val = record_curr.get_field()
                     value_val = record_curr.get_value()
 
-
                     if time_val and field_val is not None and value_val is not None:
                         records_curr.append({
                             "time": time_val,
@@ -249,7 +302,7 @@ class WeatherApp:
             df_curr = pd.DataFrame(records_curr)
 
             if not df_curr.empty:
-                df_curr['time'] = pd.to_datetime(df_curr['time'])  # Zamanı datetime formatına çevir
+                df_curr['time'] = pd.to_datetime(df_curr['time'])
                 result_current = df_curr.pivot_table(index='time', columns='field', values='value',
                                                      aggfunc='mean').reset_index()
                 print(result_current)
@@ -260,6 +313,3 @@ class WeatherApp:
 
         self.weekly_pivot_df = pivot_df
         self.current_pivot_df = result_current
-
-
-
